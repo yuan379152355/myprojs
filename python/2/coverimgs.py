b7933eb83b501be7f881c3da18e3ee9a3193499d
yuan379152355 <446968454@qq.com>
2020-05-14 19:34:33 +0800

    [commit]

--- python/2/coverimgs.py
+++ python/2/coverimgs.py
@@ -13,7 +13,7 @@ y1 = 305
 
 x3,x4,y3 = pt.getTwoHightestPoint(img_c)
 
-diff = 0
+diff =10
 rate = float((x4-x3-2*diff)/(x2-x1))
 sp = img_h.size
 img_h=img_h.resize((int(sp[0] * rate),int(sp[1]*rate)))  #,Image.BILINEAR
@@ -24,15 +24,15 @@ img_l = img_c.crop((0,0,x3, sp_c[1]))
 img_m = img_c.crop((x3, 0, x4, sp_c[1]))
 img_r = img_c.crop((x4, 0, sp_c[0], sp_c[1]))
 
-img_l.show()
-img_m.show()
-img_r.show()
+## 覆盖 衣服 短了拉升 长了压缩
+img = pt.coverImgs1(img_h, img_l, img_m, img_r, int(rate*x1), int(rate*x2), int(rate*y1), y3)
 
-img = pt.coverImgs(img_h, img_c,int(rate*x1), int(rate*x2), int(rate*y1), x3 + diff, y3)
+## 覆盖 衣服 短了拉升 长了裁剪
+#img = pt.coverImgs2(img_h, img_l, img_m, img_r, int(rate*x1), int(rate*x2), int(rate*y1), y3)
+
 
 # 回复原来的尺寸
-sp = img.size
-img=img.resize((int(sp[0] / rate),int(sp[1] / rate)))  #,Image.BILINEAR
+img=img.resize(sp)  #,Image.BILINEAR
 
 # 背景绘制颜色
 img = pt.changeBack(img, (240,0,0))
--- python/2/pictools.py
+++ python/2/pictools.py
@@ -73,7 +73,7 @@ def getTwoHightestPoint(img):
 	return hightest_x1,hightest_x2, max(hightest_y1, hightest_y2)
 
 # 覆盖图片
-def coverImgs(img, imgs,x1,x3, y1,x2,y2):
+def coverImgs2(img, imgs,x1,x3, y1,x2,y2):
 	sp=img.size
 	h1=sp[1]
 	w1=sp[0]
@@ -118,6 +118,119 @@ def coverImgs(img, imgs,x1,x3, y1,x2,y2):
 	return img
 
 # 覆盖图片
+def coverImgs1(img, img_l, img_m, img_r,x1,x3, y1,y2):
+	sp=img.size
+	h1=sp[1]
+	w1=sp[0]
+
+	# 衣服
+	sp=img_l.size
+	h2=sp[1]
+	w2_l=sp[0]
+	sp=img_r.size
+	w2_r=sp[0]
+
+	x_begin = x_end = 0
+	y_end = 0
+
+	# 获取覆盖边界，以免超过图片范围
+	# 头部左侧宽度
+	# 头部对接点以下部分高度
+	w_l = x1
+	w_r = w1 - x3
+	h_d = h1 - y1
+
+	rate_l = w_l / w2_l
+	rate_r = w_r / w2_r
+	rate_y = h_d / (h2 - y2)
+
+	h2_h = int(rate_y * h2) + 1
+	# 重置左右侧衣服尺寸
+	img_l = img_l.resize((w_l,h2_h))
+	img_m = img_m.resize((x3-x1,h2_h))
+	img_r = img_r.resize((w_r,h2_h))
+
+	pixels = img.load()
+	for yh in range(y1, h1):
+		for xw in range(0, w1):
+			# 将不为透明的点覆盖到对应位置
+			try:
+				if xw < x1:
+					color_d = img_l.getpixel((xw, h2_h - (h1-yh)))
+					pixels[xw,yh] = color_d
+				elif xw >= x3:
+					color_d = img_r.getpixel((xw - x3, h2_h - (h1-yh)))
+					pixels[xw,yh] = color_d
+				else:
+					color_d = img_m.getpixel((xw - x1, h2_h - (h1-yh)))
+					if(color_d[3] == 255):
+						pixels[xw,yh] = color_d
+			except:
+				print(xw, x1, x3, w1,img_l.size, (xw, h2_h - (h1-yh)))
+				break
+
+	return img
+
+
+# 覆盖图片
+def coverImgs2(img, img_l, img_m, img_r,x1,x3, y1,y2):
+	sp=img.size
+	h1=sp[1]
+	w1=sp[0]
+
+	# 衣服
+	sp=img_l.size
+	h2=sp[1]
+	w2_l=sp[0]
+	sp=img_r.size
+	w2_r=sp[0]
+
+	x_begin = x_end = 0
+	y_end = 0
+
+	# 获取覆盖边界，以免超过图片范围
+	# 头部左侧宽度
+	# 头部对接点以下部分高度
+	w_l = x1
+	w_r = w1 - x3
+	h_d = h1 - y1
+
+	rate_l = w_l / w2_l
+	rate_r = w_r / w2_r
+	rate_y = h_d / (h2 - y2)
+
+	diff = 0
+	if rate_y > 1:
+		h2_h = int(rate_y * h2)
+	else:
+		h2_h = h2
+		diff = (h2 - y2) - (h1 - y1)
+	# 重置左右侧衣服尺寸
+	img_l = img_l.resize((w_l,h2_h))
+	img_m = img_m.resize((x3-x1,h2_h))
+	img_r = img_r.resize((w_r,h2_h))
+
+	pixels = img.load()
+	for yh in range(y1, h1):
+		for xw in range(0, w1):
+			# 将不为透明的点覆盖到对应位置
+			try:
+				if xw < x1:
+					color_d = img_l.getpixel((xw, h2_h - (h1-yh) - diff))
+					pixels[xw,yh] = color_d
+				elif xw >= x3:
+					color_d = img_r.getpixel((xw - x3, h2_h - (h1-yh) - diff))
+					pixels[xw,yh] = color_d
+				else:
+					color_d = img_m.getpixel((xw - x1, h2_h - (h1-yh) - diff))
+					if(color_d[3] == 255):
+						pixels[xw,yh] = color_d
+			except:
+				print(xw, x1, x3, w1,img_l.size, (xw, h2_h - (h1-yh) - diff))
+				break
+
+	return img
+
 def changeBack(img, color):
 	x,y = img.size
 
